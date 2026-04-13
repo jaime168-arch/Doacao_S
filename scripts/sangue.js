02bd48e752a440deeca06dd356dfa3327133940a
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-04-13 09:50:01 -0300

    add- ajustes no código

--- scripts/sangue.js
+++ scripts/sangue.js
@@ -18,8 +18,25 @@ class GerenciadorDoadores {
         e.preventDefault();
 
         const novoDoador = {
-            nome: document.getElementById('nome').ariaValueMax,
-            
+            nome: document.getElementById('nome').Value,
+            idade: parseInt(document.getElementById('age').Value),
+            peso: parseFloat(document.getElementById('peso').Value)
+            tipo: document.getElementById('tipov sanguineo').Value,
+            local: `${document.getElementById('cidade'). Value}/${document.getElementById('Estado').value.toUpperCase()}`
+        };
+
+        if (novoDoador.peso < 50 || novoDoador.idade < 16) {
+            this.notificar("Requisitos básicos não atendidos (Peso/Idade).", "erro");
+            return;
         }
+
+        this.doadores.push(novoDoador);
+        this.salvar();
+        this.notificar("Cadastro realizado!", "sucesso");
+        this.form.reset();
+    }
+
+    salvar() {
+        localStorage.setItem('doadores_db', JSON.stringify(this.doadores));
     }
 }
