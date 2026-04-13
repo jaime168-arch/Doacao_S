6c2652a357a1768950f37a0d06713dca42957831
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-04-13 09:36:27 -0300

    add- init e cadastro

--- scripts/sangue.js
+++ scripts/sangue.js
@@ -8,4 +8,18 @@ class GerenciadorDoadores {
 
         this.init();
     }
+
+    init() {
+        this.form.addEventListener('submit' , (e) => this.cadastrar(e));
+        this.renderizarTabela();
+    }
+
+    cadastrar(e){
+        e.preventDefault();
+
+        const novoDoador = {
+            nome: document.getElementById('nome').ariaValueMax,
+            
+        }
+    }
 }
